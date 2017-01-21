1376c17cebfe4884320b7bb31337c273be017369
Andrew McNutt <mcnutt.andrew@gmail.com>
2017-01-20 16:08:47 -0800

    Add labels to radial plot (#249)
    * Add labels to radial plot
    
    * remove unused scss
    
    * respond to code review
    
    * add explanations for the math

--- showcase/radial-chart/custom-radius-radial-chart.js
+++ showcase/radial-chart/custom-radius-radial-chart.js
@@ -33,6 +33,7 @@ export default class SimpleRadialChart extends Component {
     return (
       <RadialChart
         animation
+        showLabels
         radiusDomain={[0, 20]}
         data={[
           {
@@ -43,6 +44,8 @@ export default class SimpleRadialChart extends Component {
           },
           {
             angle: 2,
+            label: 'Super Custom label',
+            subLabel: 'With annotation',
             id: 2,
             radius: 20,
             opacity: (!hoveredSection || hoveredSection === 2) ? 1 : 0.6
@@ -51,7 +54,8 @@ export default class SimpleRadialChart extends Component {
             angle: 5,
             id: 3,
             radius: 5,
-            opacity: (!hoveredSection || hoveredSection === 3) ? 1 : 0.6
+            opacity: (!hoveredSection || hoveredSection === 3) ? 1 : 0.6,
+            label: 'Alt Label'
           },
           {
             angle: 3,
@@ -63,12 +67,13 @@ export default class SimpleRadialChart extends Component {
             angle: 5,
             id: 5,
             radius: 12,
+            subLabel: 'Sub Label only',
             opacity: (!hoveredSection || hoveredSection === 5) ? 1 : 0.6
           }
         ]}
         onSectionMouseOver={row => this.setState({hoveredSection: row.id})}
         onSectionMouseOut={() => this.setState({hoveredSection: false})}
-        width={300}
+        width={600}
         height={300} />
     );
   }
