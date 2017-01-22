fea4816f4c8006e53caf13b3a464ca53d9936f27
Andrew Mc Nutt <mcnutt@uber.com>
2017-01-21 21:34:01 -0800

    Initial Pass as responsive vis

---
+++ examples/responsive-vis/responsive-scatterplot.js
@@ -0,0 +1,165 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import React from 'react';
+
+import {
+  XYPlot,
+  MarkSeries,
+  HeatmapSeries,
+  XAxis,
+  YAxis,
+  Hint
+} from 'react-vis';
+
+import LabelSeries from './label-series';
+import {
+  filterFeatures,
+  computeRadius,
+  getPPP,
+  transformToBinData,
+  manicureData
+} from './responsive-vis-utils';
+
+// range constants
+const SUPER_LOW_RANGE = [0, 1e-4];
+const VERY_LOW_RANGE = [0, 8e-4];
+const LOW_RANGE = [0, 5e-3];
+const MED_LOW_RANGE = [1e-4, 5e-3];
+const HIGH_LOW_RANGE = [8e-4, 5e-3];
+const MED_RANGE = [5e-3, 1e-2];
+const HIGH_RANGE = [1e-2, Infinity];
+const MED_HIGH_RANGE = [MED_RANGE[0], HIGH_RANGE[1]];
+// const MIN_PPP = 2e-5;
+
+const SCATTERPLOT_FEATURES = [
+  {min: -Infinity, max: Infinity, name: 'axes'},
+  {min: SUPER_LOW_RANGE[0], max: SUPER_LOW_RANGE[1], name: 'labels'},
+  {min: VERY_LOW_RANGE[0], max: VERY_LOW_RANGE[1], name: 'pointSelection'},
+  {min: LOW_RANGE[0], max: LOW_RANGE[1], name: 'points'},
+  {min: MED_LOW_RANGE[0], max: MED_LOW_RANGE[1], name: 'tooltips'},
+  {min: MED_HIGH_RANGE[0], max: MED_HIGH_RANGE[1], name: 'bins'},
+  {min: MED_HIGH_RANGE[0], max: MED_HIGH_RANGE[1], name: 'bintips'},
+  {min: MED_HIGH_RANGE[0], max: MED_HIGH_RANGE[1], name: 'binSelection'},
+  {min: HIGH_LOW_RANGE[0], max: HIGH_LOW_RANGE[1], name: 'brushSelection'}
+];
+
+export default class ResponsiveScatterplot extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this._rememberValue = this._rememberValue.bind(this);
+    this._forgetValue = this._forgetValue.bind(this);
+    this._select = this._select.bind(this);
+    this._selectValue = this._selectValue.bind(this);
+    this._selectBin = this._selectBin.bind(this);
+  }
+
+  state = {
+    binData: [],
+    hoveredPoint: false,
+    selectedPoints: []
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // not the greatest
+    this.setState({
+      binData: transformToBinData(nextProps.data, nextProps.width, nextProps.height)
+    });
+  }
+
+  _rememberValue(value) {
+    this.setState({hoveredPoint: value});
+  }
+
+  _forgetValue() {
+    this.setState({hoveredPoint: null});
+  }
+
+  _select(value, accessor) {
+    let foundValue = false;
+
+    const selectedPoints = this.state.selectedPoints.filter(row => {
+      if (accessor(row) === accessor(value)) {
+        foundValue = true;
+      }
+      return accessor(row) !== accessor(value);
+    });
+
+    if (!foundValue) {
+      selectedPoints.push(value);
+    }
+
+    this.setState({selectedPoints});
+  }
+
+  _selectBin(value, e) {
+    e.event.stopPropagation();
+    this._select(value, d => `${d.x}-${d.y}`);
+  }
+
+  _selectValue(value, e) {
+    e.event.stopPropagation();
+    this._select(value, d => d.label);
+  }
+
+  render() {
+    const {binData, hoveredPoint, selectedPoints} = this.state;
+    const {data, height, width} = this.props;
+    const ppp = getPPP(width, height, data, 'TWOD');
+    const featuresToRender = filterFeatures(SCATTERPLOT_FEATURES, ppp);
+    const remeberVal = featuresToRender.pointSelection || featuresToRender.tooltips;
+    const remeberBin = featuresToRender.bintips || featuresToRender.binSelection;
+
+    const pointRadii = computeRadius(data, width, height);
+    return (
+      <div className="responsive-vis">
+        <XYPlot
+          height={height}
+          width={width}>
+          {featuresToRender.axes && <XAxis />}
+          {featuresToRender.axes && <YAxis />}
+          {featuresToRender.bins && <HeatmapSeries
+            className="responsive-vis-heatmap"
+            colorType="literal"
+            onValueMouseOver={remeberBin ? this._rememberValue : null}
+            onValueMouseOut={remeberBin ? this._forgetValue : null}
+            onValueClick={featuresToRender.binSelection ? this._selectBin : null}
+            data={manicureData(binData, hoveredPoint, selectedPoints, true)}/>}
+          {featuresToRender.points && <MarkSeries
+            className="responsive-vis-scatterplot"
+            colorType="literal"
+            size={pointRadii}
+            onValueMouseOver={remeberVal ? this._rememberValue : null}
+            onValueMouseOut={remeberVal ? this._forgetValue : null}
+            onValueClick={featuresToRender.pointSelection ? this._selectValue : null}
+            data={manicureData(data, hoveredPoint, selectedPoints, false)}/>}
+          {(featuresToRender.tooltips || featuresToRender.bintips) &&
+            hoveredPoint && <Hint value={hoveredPoint}/>}
+          {featuresToRender.labels && <LabelSeries
+            allowOffsetToBeReversed
+            data={data}
+            yOffset={-1 * pointRadii}
+            />}
+        </XYPlot>
+      </div>
+    );
+  }
+}
---
+++ examples/responsive-vis/responsive-vis-utils.js
@@ -0,0 +1,110 @@
+import {randomNormal} from 'd3-random';
+import {scaleLinear, scaleQuantize, scaleSqrt} from 'd3-scale';
+import {max, range} from 'd3-array';
+
+// create fake data
+export function createData(dataSize) {
+  const gen1 = randomNormal(70, 15);
+  const gen2 = randomNormal(50, 8);
+  const gen = (i) => i % 3 ? gen1() : gen2();
+  const data = new Array(dataSize);
+  let index = -1;
+
+  while (index++ < dataSize) {
+    data[index] = {
+      x: gen(index),
+      y: gen(index),
+      label: `Point ${index}`,
+      color: '#12939A'
+    };
+  }
+
+  return data;
+}
+
+export function filterFeatures(features, ppp) {
+  return features.reduce((res, feature) => {
+    if (ppp < feature.min || ppp > feature.max) {
+      return res;
+    }
+    res[feature.name] = true;
+    return res;
+  }, {});
+}
+
+export function computeRadius(data, width, height) {
+  const targetPPP = 2e-2;
+  const radiusDomain = [2, 12];
+  const r = Math.sqrt(targetPPP * width * height / data.length);
+  return Math.min(radiusDomain[1], Math.max(radiusDomain[0], r));
+}
+
+export function getPPP(w, h, data, dimensionality) {
+  // what is TWOD?
+  const pixels = dimensionality === 'TWOD' ? w * h : dimensionality === 'WIDTH' ? w : h;
+  return data.length / pixels;
+}
+
+function generateDomain(data) {
+  return data.reduce((res, row) => {
+    res = {
+      xMin: Math.min(res.xMin, row.x),
+      xMax: Math.max(res.xMax, row.x),
+      yMin: Math.min(res.yMin, row.y),
+      yMax: Math.max(res.yMax, row.y)
+    };
+    return res;
+  }, {xMin: Infinity, xMax: -Infinity, yMin: Infinity, yMax: -Infinity});
+}
+
+export function transformToBinData(data, width, height) {
+  const targetPPP = 5e-3;
+  const binCount = ~~Math.sqrt(width * height * targetPPP);
+  const domains = generateDomain(data);
+  const xBin = scaleQuantize().domain([domains.xMin, domains.xMax]).range(range(binCount));
+  const yBin = scaleQuantize().domain([domains.yMin, domains.yMax]).range(range(binCount));
+
+  const binData = new Array(binCount * binCount);
+  for (let i = 0; i < binData.length; i++) {
+    binData[i] = 0;
+  }
+  data.forEach(d => {
+    const index = xBin(d.x) + (binCount * yBin(d.y));
+    binData[index]++;
+  });
+  const maxCount = max(binData);
+  const color = scaleSqrt().domain([0, maxCount]).range(['#fff', '#12939A']);
+
+  const mappedData = binData.map((d, index) => {
+    const x = index % binCount;
+    const y = ~~(index / binCount);
+    return {x, y, color: color(d), count: d, maxCount};
+  });
+  return mappedData;
+}
+
+function transformColor(data, hovered = [], useRange = false) {
+  const samplePoint = data[0];
+  const color = useRange ?
+    scaleLinear().domain([0, samplePoint.maxCount]).range(['#fff', '#EF5D28']) :
+    d => '#EF5D28';
+
+  const hoveredPoints = hovered.reduce((res, row) => {
+    res[useRange ? `${row.x}-${row.y}` : row.label] = true;
+    return res;
+  }, {});
+  return data.map(row => {
+    if (hoveredPoints[useRange ? `${row.x}-${row.y}` : row.label]) {
+      return {...row, color: color(row.count)};
+    }
+    return row;
+  });
+}
+
+export function manicureData(data, hoveredPoint, selectedPoints, useRange) {
+  if (!hoveredPoint && selectedPoints.length === 0) {
+    return data;
+  }
+  const concatedData = !hoveredPoint ? selectedPoints : selectedPoints.concat([hoveredPoint]);
+  return transformColor(data, concatedData, useRange);
+}
