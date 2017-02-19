5e1afa64d815c1d1ca60203a8ae8693670b7039e
Andrew Mc Nutt <mcnutt@uber.com>
2017-02-19 15:51:03 -0800

    add example verifying that sankey can take alternate opacities

--- showcase/sankey/basic.js
+++ showcase/sankey/basic.js
@@ -4,7 +4,7 @@ import Sankey from 'sankey';
 
 const nodes = [{name: 'a'}, {name: 'b'}, {name: 'c'}];
 const links = [
-  {source: 0, target: 1, value: 10},
+  {source: 0, target: 1, value: 10, opacity: 0.2},
   {source: 0, target: 2, value: 20},
   {source: 1, target: 2, value: 20}
 ];
