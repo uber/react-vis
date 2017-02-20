0f154ae054f593e5ea5096cec1e84b059a00cbfb
Andrew Mc Nutt <mcnutt@uber.com>
2017-02-19 23:01:40 -0800

    Enforce a minimum number of boxes in the complex chart

--- showcase/plot/complex-chart.js
+++ showcase/plot/complex-chart.js
@@ -46,7 +46,7 @@ function getRandomSeriesData(total) {
   let lastY = Math.random() * 40 - 20;
   let y;
   const firstY = lastY;
-  for (let i = 0; i < total; i++) {
+  for (let i = 0; i < Math.max(total, 3); i++) {
     y = Math.random() * firstY - firstY / 2 + lastY;
     result.push({
       x: i,
