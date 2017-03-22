7469b55592b6802fc301364289fcfa4946b17e36
Andrew Mc Nutt <mcnutt@uber.com>
2017-03-22 15:57:30 -0700

    update

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cd docs &&
-npm run build &&
-cp -rf docs-dist/ .. &&
-rm -rf docs-dist &&
-cd .. &&
-git checkout local-gh-pages &&
-git add --a &&
+cd docs
+npm run build
+cp -rf docs-dist/ ..
+rm -rf docs-dist
+cd ..
+git checkout local-gh-pages
+mv docs-dist/bundle.js ./bundle.js
+git add --a
 git commit -m 'Upgrade docs'
