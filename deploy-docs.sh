5bcda4c7b2e9e0ae8fa1eb994d432368d9d7ba4f
Andrew Mc Nutt <mcnutt@uber.com>
2017-03-22 16:05:27 -0700

    mod script

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -3,9 +3,11 @@
 cd docs
 npm run build
 cp -rf docs-dist/ ..
+cp -rf docs/markdown public/
 rm -rf docs-dist
 cd ..
 git checkout local-gh-pages
 mv docs-dist/bundle.js ./bundle.js
+mv public/markdown markdown
 git add --a
 git commit -m 'Upgrade docs'
