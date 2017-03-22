64b0394d667e99602d1c785bb897b457ded4b944
Andrew Mc Nutt <mcnutt@uber.com>
2017-03-22 16:54:18 -0700

    one last

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -6,8 +6,10 @@ npm run build &&
 mv docs-dist/ ../ &&
 cp -rf ./markdown ../docs-dist/markdown &&
 cd .. &&
-git checkout local-gh-pages &&
+git checkout gh-pages &&
 mv docs-dist/bundle.js ./bundle.js &&
 mv docs-dist/markdown ./ &&
 git add --a &&
-git commit -m 'Upgrade docs'
+git commit -m 'Upgrade docs' &&
+git push &&
+git checkout master
