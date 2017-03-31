9419f70751fe107259ec2664c9a1f9e1fa8b2b26
Andrew Mc Nutt <mcnutt@uber.com>
2017-03-31 15:06:10 -0700

    Allow examples to have associated markdown files

--- docs/docs-app/actions/index.js
+++ docs/docs-app/actions/index.js
@@ -1,19 +1,27 @@
 import {text} from 'd3-request';
-import {docPages} from '../constants/pages';
+import {docPages, examplePages} from '../constants/pages';
 export function loadContent() {
   return (dispatch, getState) => {
-    docPages.forEach(documentationSection => {
-      documentationSection.children.forEach(docPage => {
-        const filename = docPage.content.markdown;
-        const contents = getState();
-        if (contents.markdownPages.get(filename)) {
-          // already loaded
-          return;
-        }
-        dispatch(loadContentStart(filename));
-        text(filename, (error, response) => {
-          dispatch(loadContentSuccess(filename, error ? error.target.response : response));
-        });
+    docPages.forEach(loadDoc(dispatch, getState));
+    examplePages.forEach(loadDoc(dispatch, getState));
+  };
+}
+
+function loadDoc(dispatch, getState) {
+  return documentationSection => {
+    documentationSection.children.forEach(docPage => {
+      const filename = docPage.content.markdown;
+      if (!filename) {
+        return;
+      }
+      const contents = getState();
+      if (contents.markdownPages.get(filename)) {
+        // already loaded
+        return;
+      }
+      dispatch(loadContentStart(filename));
+      text(filename, (error, response) => {
+        dispatch(loadContentSuccess(filename, error ? error.target.response : response));
       });
     });
   };
--- docs/docs-app/components/documentation-page.js
+++ docs/docs-app/components/documentation-page.js
@@ -1,29 +1,5 @@
 import React, {Component} from 'react';
-import marked from 'marked';
-
-import {docsRouting} from '../constants/pages';
-import {showCase} from '../../../showcase/index';
-
-const INJECTION_REG = /<!-- INJECT:"(.+)\" -->/g;
-
-function injectExamplesIntoHtml(content) {
-  return content.split(INJECTION_REG).map((__html, index) => {
-    const Example = showCase[__html];
-    if (!Example) {
-      /* eslint-disable react/no-danger */
-      return (<div
-        className="markdown-body"
-        key={`body-${index}`}
-        dangerouslySetInnerHTML={{__html}} />);
-      /* eslint-enable react/no-danger */
-    }
-    return (
-      <div className="markdown-example" key={`example-${index}`}>
-        <Example />
-      </div>
-    );
-  });
-}
+import {injectExamplesIntoHtml, convertMarkdownToReact} from './utils';
 
 class DocumentationPage extends Component {
   componentWillReceiveProps(nextProps) {
@@ -34,23 +10,12 @@ class DocumentationPage extends Component {
 
   render() {
     const {markdownPages, route} = this.props;
-
-    const renderer = new marked.Renderer();
-    renderer.link = (href, title, text) => {
-
-      if (docsRouting[href]) {
-        return `<a href=${docsRouting[href]} title=${title}>${text}</a>`;
-      }
-      return `<a href=${href} title=${title}>${text}</a>`;
-    };
-
     const content = markdownPages.get(route.content.markdown, '');
-    const __html = marked(content, {renderer});
 
     return (
       <div className="documentation-page f fg">
         <div className="markdown" ref="documentionContainer">
-          {injectExamplesIntoHtml(__html)}
+          {injectExamplesIntoHtml(convertMarkdownToReact(content))}
         </div>
       </div>
     );
