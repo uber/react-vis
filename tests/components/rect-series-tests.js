d9ad81d6ba219c7422428cbf618c72c5b80c2a24
Andrew McNutt <mcnutt.andrew@gmail.com>
2017-05-23 14:05:39 -0700

    More expansive histogram example

--- tests/components/rect-series-tests.js
+++ tests/components/rect-series-tests.js
@@ -4,6 +4,7 @@ import {mount} from 'enzyme';
 import HorizontalRectSeries from 'plot/series/horizontal-bar-series';
 import VerticalRectSeries from 'plot/series/vertical-bar-series';
 import {testRenderWithProps, GENERIC_XYPLOT_SERIES_PROPS} from '../test-utils';
+import Histogram from '../../showcase/plot/histogram';
 import StackedHistogram from '../../showcase/plot/stacked-histogram';
 
 testRenderWithProps(HorizontalRectSeries, GENERIC_XYPLOT_SERIES_PROPS);
@@ -17,5 +18,13 @@ test('RectSeries: Showcase Example - StackedHistogram', t => {
   $.find('.showcase-button').simulate('click');
   t.equal($.find('.rv-xy-plot__series--rect rect').length, 0, 'should now find no rects');
   t.equal($.find('.rv-xy-canvas canvas').length, 1, 'should now find one canvas');
+  t.end();
+});
+
+test('RectSeries: Showcase Example - Histogram', t => {
+  const $ = mount(<Histogram />);
+  t.equal($.text(), 'May 21May 28Jun 04Jun 11Jun 180.00.51.01.52.0', 'should fine the right text content');
+  t.equal($.find('.rv-xy-plot__series--rect rect').length, 8, 'should find the right number of bars');
+
   t.end();
 });
