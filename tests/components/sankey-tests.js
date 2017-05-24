346cb1ac9f9afb3992811cfd515572de377e9b59
Balthazar Gronon <balthazar@uber.com>
2017-05-24 15:07:24 -0700

    chore(sankey): address comments :cat:

--- tests/components/sankey-tests.js
+++ tests/components/sankey-tests.js
@@ -1,6 +1,7 @@
 import test from 'tape';
 import React from 'react';
 import {mount} from 'enzyme';
+
 import Sankey from 'sankey';
 import BasicSankey from '../../showcase/sankey/basic';
 import VoronoiSankey from '../../showcase/sankey/voronoi';
@@ -16,6 +17,23 @@ import {testRenderWithProps} from '../test-utils';
 
 // make sure that the components render at all
 testRenderWithProps(Sankey, SANKEY_PROPS);
+
+test('Sankey: labels', t => {
+  const wrap = mount(
+    <Sankey
+      height={100}
+      width={100}
+      nodes={[{name: 'one'}, {name: 'two'}]}
+      links={[{source: 0, target: 1}]} />
+  );
+
+  t.equal(wrap.find('text').length, 2, 'there should be two node labels');
+  wrap.setProps({hideLabels: true});
+  t.equal(wrap.find('text').length, 0, 'the labels should now be hidden');
+
+  t.end();
+
+});
 
 test('Sankey: Showcase Example - BasicSankey', t => {
   const $ = mount(<BasicSankey />);
