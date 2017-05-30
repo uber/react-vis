99af4e9e094f6d0d0cde44413c0eccf19714ca2b
Austin Kindred <akindr@users.noreply.github.com>
2017-05-30 11:35:41 -0400

    Zoom component (#405)
    An example of an interactive `Highlight` component. On mouse up, the chart then zooms to the specified domain.
    
    This gives an idea of how to make a custom `Series` and leverage the mouse events provided by the  plot.

---
+++ examples/zoomable-chart/README.md
@@ -0,0 +1,11 @@
+react-vis zoomable-chart example
+================================
+
+## Setup
+```bash
+git clone https://github.com/uber/react-vis
+cd react-vis/examples/
+npm install
+cd zoomable-chart
+npm start
+```
---
+++ examples/zoomable-chart/zoomable-chart-example.js
@@ -0,0 +1,138 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import React from 'react';
+
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  makeWidthFlexible,
+  LineSeries,
+  DiscreteColorLegend
+} from 'react-vis';
+import Highlight from './highlight';
+
+const FlexibleXYPlot = makeWidthFlexible(XYPlot);
+
+const totalValues = 100;
+
+/**
+ * Get the array of x and y pairs.
+ * The function tries to avoid too large changes of the chart.
+ * @param {number} total Total number of values.
+ * @returns {Array} Array of data.
+ * @private
+ */
+function getRandomSeriesData(total) {
+  const result = [];
+  let lastY = Math.random() * 40 - 20;
+  let y;
+  const firstY = lastY;
+  for (let i = 0; i < total; i++) {
+    y = Math.random() * firstY - firstY / 2 + lastY;
+    result.push({
+      x: i,
+      y
+    });
+    lastY = y;
+  }
+  return result;
+}
+
+export default class ZoomableChartExample extends React.Component {
+
+  state = {
+    lastDrawLocation: null,
+    series: [
+      {
+        title: 'Apples',
+        disabled: false,
+        data: getRandomSeriesData(totalValues)
+      },
+      {
+        title: 'Bananas',
+        disabled: false,
+        data: getRandomSeriesData(totalValues)
+      }
+    ]
+  }
+
+  render() {
+    const {series, lastDrawLocation} = this.state;
+    return (
+      <div className="example-with-click-me">
+        <div className="legend">
+          <DiscreteColorLegend
+            width={180}
+            items={series}/>
+        </div>
+
+        <div className="chart no-select">
+          <FlexibleXYPlot
+            animation
+            xDomain={lastDrawLocation && [lastDrawLocation.left, lastDrawLocation.right]}
+            height={300}>
+
+            <HorizontalGridLines />
+
+            <YAxis />
+            <XAxis />
+
+            {series.map(entry => (
+              <LineSeries
+                key={entry.title}
+                data={entry.data}
+              />
+            ))}
+
+            <Highlight onBrushEnd={(area) => {
+              this.setState({
+                lastDrawLocation: area
+              });
+            }} />
+
+          </FlexibleXYPlot>
+        </div>
+
+        <button className="showcase-button" onClick={() => {
+          this.setState({lastDrawLocation: null});
+        }}>
+          Reset Zoom
+        </button>
+
+        <div>
+          <h4>
+            <b>Last Draw Area</b>
+          </h4>
+          {lastDrawLocation ? (
+            <ul style={{listStyle: 'none'}}>
+              <li><b>Top:</b> {lastDrawLocation.top}</li>
+              <li><b>Right:</b> {lastDrawLocation.right}</li>
+              <li><b>Bottom:</b> {lastDrawLocation.bottom}</li>
+              <li><b>Left:</b> {lastDrawLocation.left}</li>
+            </ul>
+          ) : <span>N/A</span>}
+        </div>
+      </div>
+    );
+  }
+}
