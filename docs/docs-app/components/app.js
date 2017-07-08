400b0426c9b9063e7a2b74b5292214a49a4c8cf4
Jerome Cukier <jerome@uber.com>
2017-07-07 17:46:11 -0700

    updated header

--- docs/docs-app/components/app.js
+++ docs/docs-app/components/app.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Header from './header';
 
 /* eslint-disable react/display-name*/
-export default ({children}) => (
-  <div className="app">
-    <Header />
-    <div className="app-container">
-      {children}
-    </div>
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      opacity: 1,
+      isMenuOpen: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+  toggleMenu() {
+    this.setState({isMenuOpen: !this.state.isMenuOpen});
+  }
+  render() {
+    return (<div className="app">
+      <Header
+        isMenuOpen={this.state.isMenuOpen}
+        opacity={this.state.opacity}
+        toggleMenu={this.toggleMenu}
+      />
+      <div className="app-container">
+        {this.props.children}
+      </div>
+    </div>);
+  }
+}
 /* eslint-enable react/display-name*/
+
+export default App;
