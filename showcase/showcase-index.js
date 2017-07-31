82aabaee7dec7c9b9c4c3ed3cb41a17984d57672
Andrew McNutt <mcnutt.andrew@gmail.com>
2017-07-31 14:12:49 -0700

    remove unused code

--- showcase/showcase-index.js
+++ showcase/showcase-index.js
@@ -13,39 +13,3 @@ export ForceDirectedGraph from './examples/force-directed-graph/force-directed-e
 export ResponsiveVis from './examples/responsive-vis/responsive-vis-example';
 export StreamgraphExample from './examples/streamgraph/streamgraph-example';
 export ZoomableChart from './examples/zoomable-chart/zoomable-chart-example';
-//
-// import AxesShowcase from './showcase-sections/axes-showcase';
-// import PlotsShowcase from './showcase-sections/plots-showcase';
-// import SunburstSection from './showcase-sections/sunburst-showcase';
-// import RadialShowcase from './showcase-sections/radial-showcase';
-// import RadarShowcase from './showcase-sections/radar-showcase';
-// import LegendsShowcase from './showcase-sections/legends-showcase';
-// import SankeysShowcase from './showcase-sections/sankeys-showcase';
-// import TreemapShowcase from './showcase-sections/treemap-showcase';
-// import MiscShowcase from './showcase-sections/misc-showcase';
-//
-// import Candlestick from './examples/candlestick/candlestick-example';
-// import ForceDirectedGraph from './examples/force-directed-graph/force-directed-example';
-// import ResponsiveVis from './examples/responsive-vis/responsive-vis-example';
-// import StreamgraphExample from './examples/streamgraph/streamgraph-example';
-// import ZoomableChart from './examples/zoomable-chart/zoomable-chart-example';
-//
-// const showcasesWrapper = {
-//   AxesShowcase,
-//   PlotsShowcase,
-//   SunburstSection,
-//   RadialShowcase,
-//   RadarShowcase,
-//   LegendsShowcase,
-//   SankeysShowcase,
-//   TreemapShowcase,
-//   MiscShowcase,
-//
-//   Candlestick,
-//   ForceDirectedGraph,
-//   ResponsiveVis,
-//   StreamgraphExample,
-//   ZoomableChart
-// };
-//
-// export default showcasesWrapper;
