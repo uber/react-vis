728b447480e6683861706424f79dc6063e76baec
Andrew McNutt <mcnutt.andrew@gmail.com>
2017-08-31 12:40:52 -0700

    fix bar chart

--- showcase/plot/bar-chart.js
+++ showcase/plot/bar-chart.js
@@ -30,31 +30,6 @@ import {
   VerticalBarSeriesCanvas
 } from 'index';
 
-const myDATA = [
-  {id: '00036', y: 200400, x: 1504121437},
-  {id: '00036', y: 200350, x: 1504121156},
-  {id: '00036', y: 200310, x: 1504120874},
-  {id: '00036', y: 200260, x: 1504120590},
-  {id: '00036', y: 200210, x: 1504120306},
-  {id: '00036', y: 200160, x: 1504120024},
-  {id: '00036', y: 200120, x: 1504119740},
-  {id: '00036', y: 200070, x: 1504119458},
-  {id: '00036', y: 200020, x: 1504119177},
-  {id: '00036', y: 199980, x: 1504118893},
-  {id: '00036', y: 199930, x: 1504118611},
-  {id: '00036', y: 199880, x: 1504118330},
-  {id: '00036', y: 199830, x: 1504118048},
-  {id: '00036', y: 199790, x: 1504117763},
-  {id: '00036', y: 199740, x: 1504117481}
-];
-
-const yDomain = myDATA.reduce((res, row) => {
-  return {
-    max: Math.max(res.max, row.y),
-    min: Math.min(res.min, row.y)
-  };
-}, {max: -Infinity, min: Infinity});
-
 export default class Example extends React.Component {
   state = {
     useCanvas: false
@@ -70,17 +45,28 @@ export default class Example extends React.Component {
           onClick={() => this.setState({useCanvas: !useCanvas})}
           buttonContent={content}/>
         <XYPlot
-          xType="time"
+          xType="ordinal"
           width={300}
           height={300}
-          yDomain={[yDomain.min, yDomain.max]}
-          yBaseValue={yDomain.min}
+          xDistance={100}
           >
-          <BarSeries
-            className="vertical-bar-series-example"
-            data={myDATA}/>
+          <VerticalGridLines />
+          <HorizontalGridLines />
           <XAxis />
           <YAxis />
+          <BarSeries
+            className="vertical-bar-series-example"
+            data={[
+              {x: 'A', y: 10},
+              {x: 'B', y: 5},
+              {x: 'C', y: 15}
+            ]}/>
+          <BarSeries
+            data={[
+              {x: 'A', y: 12},
+              {x: 'B', y: 2},
+              {x: 'C', y: 11}
+            ]}/>
         </XYPlot>
       </div>
     );
