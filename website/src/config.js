7ad8ae1395538f4858b1529ce902af6f2be32ce5
Jerome Cukier <jerome@uber.com>
2017-10-02 17:50:34 -0700

    fixed header menu

--- website/src/config.js
+++ website/src/config.js
@@ -5,7 +5,12 @@ export const PROJECT_ORG = 'uber';
 export const PROJECT_URL = `https://github.com/${PROJECT_ORG}/${PROJECT_NAME}`;
 export const PROJECT_DESC = 'A composable charting library';
 
-export const PROJECTS = {};
+export const PROJECTS = {
+  'deck.gl': 'https://uber.github.io/deck.gl',
+  'luma.gl': 'https://uber.github.io/luma.gl',
+  'react-map-gl': 'https://uber.github.io/react-map-gl',
+  'vis-academy': 'https://uber.github.io/vis-academy',
+};
 
 export const HOME_HEADING = 'A composable charting library';
 
