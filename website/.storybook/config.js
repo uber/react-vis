6c4692eb352ccac3f22eb6c29e0d02177b2c4662
Jerome Cukier <jerome@uber.com>
2018-03-21 11:49:49 -0700

    adding storybook configuration and test story

---
+++ website/.storybook/config.js
@@ -0,0 +1,8 @@
+import { configure } from '@storybook/react';
+
+function loadStories() {
+  require('../storybook/index.js');
+  // You can require as many stories as you need.
+}
+
+configure(loadStories, module);
