de0bce13d15b3449cd573bd871f77212e862d1f0
Amelia Wattenberger <wattenberger@gmail.com>
2018-10-07 22:55:31 -0400

    update tests for discrete color legend
    update tests to use svg properties, add tests for stroke dasharray and stroke width, and filter out unused styles

--- src/legends/discrete-color-legend-item.js
+++ src/legends/discrete-color-legend-item.js
@@ -46,6 +46,7 @@ function DiscreteColorLegendItem({
   if (onClick) {
     className += ' clickable';
   }
+  const strokeDasharrayStyle = STROKE_STYLES[strokeStyle] || strokeDasharray;
   return (
     <div {...{className, onClick, onMouseEnter, onMouseLeave}}>
       <svg className="rv-discrete-color-legend-item__color" height={2} width={14}>
@@ -53,8 +54,8 @@ function DiscreteColorLegendItem({
           className="rv-discrete-color-legend-item__color__path"
           d="M 0, 1 L 14, 1"
           style={{
-            strokeWidth,
-            strokeDasharray: STROKE_STYLES[strokeStyle] || strokeDasharray,
+            ...(strokeWidth ? {strokeWidth} : {}),
+            ...(strokeDasharrayStyle ? {strokeDasharray: strokeDasharrayStyle} : {}),
             stroke: disabled ? null : color
           }}
 
