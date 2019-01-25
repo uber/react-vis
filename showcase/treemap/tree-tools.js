5cba103702621303f48a77de652403ad560052df
Alexander Dubinin <akiyamka@gmail.com>
2019-01-25 11:36:58 +0300

    add more comments for tree-tools

--- showcase/treemap/tree-tools.js
+++ showcase/treemap/tree-tools.js
@@ -1,10 +1,13 @@
 /**
  * Find branch in tree by omen pair - key-value
+ * @author Akiyamka <akiyamka@gmail.com>
+ *
  * @param {Object} omen - how to find branch. Example - { id: 123456 }
  * @param {Object} options [{children: 'children', returnChild: true}];
- * @param {Boolean} options.returnChild -
+ * @param {Boolean} options.returnChild - return whole branch or not
  * if true return branch started from found child,
- * else return whole branch where child founded
+ * @returns {Object} if options.returnChild true - return branch started from found child,
+ * else - return whole branch where child founded
  */
 export function findBranchByOmen(omen, options) {
   const [[omenProp, omenValue]] = Object.entries(omen);
@@ -26,14 +29,18 @@ export function findBranchByOmen(omen, options) {
 }
 
 /**
- * Fold childrens, and recalculate weights on leafs
+ * Fold childrens, and recalculate weights on leafs.
+ * Used to create tree slice of the required depth
+ * @author Akiyamka <akiyamka@gmail.com>
+ *
  * @param {Object} tree
- * @param {Object} options [{
- *    children: 'children',
- *    value: 'value',
- *    maxLevel: 2,
- *    clearValues: false - if true clear values from branchs (if they not leaf)
- *  }]
+ * @param {Object} options
+ * @param {String} options.children - the field where to take children
+ * @param {String} options.value - the field where the values will be written
+ * @param {String} options.maxLevel - the maximum depth of leaf
+ * @param {String} options.clearValues [false] - if true - values saved only on leafs
+ *
+ * @returns {Object} folded tree
  */
 export function foldChilndrens(tree, options, level = 0) {
   const opt = {
@@ -49,15 +56,15 @@ export function foldChilndrens(tree, options, level = 0) {
       chld[opt.children]
       ? calcLeafWeight(chld[opt.children])
       : chld[opt.value]
-    ) + summ, 0);
+    ) + summ,
+  0);
 
-  if (
-    !tree[opt.children]
-    || tree[opt.children].length === 0
-  ) return tree;
+  const isNotHaveChildren = !tree[opt.children] || tree[opt.children].length === 0;
+  if (isNotHaveChildren) {
+    return tree
+  };
 
   if (level < opt.maxLevel) {
-    // Delete value from tree;
     const getWithout = (data, exception) => {
       const {[exception]: omit, ...rest} = data; // eslint-disable-line no-unused-vars
       return rest;
@@ -74,13 +81,24 @@ export function foldChilndrens(tree, options, level = 0) {
   return leaf;
 }
 
+/**
+ * Generate unique color for item based on his name and value.
+ * (Value affects transparency, name affects color)
+ * @param {Object} item
+ * @param {String} item.name
+ * @param {Number} item.value
+ * @param {Number} s [80] - saturation
+ * @param {Number} l [50] - lightness
+ *
+ * @returns {String} in css format: hsla(H,S,L,A)
+ */
 export function colorFromValue(item, s = 80, l = 50) {
   const str = item.name;
-  const val = item.value || 1;
+  const val = item.value;
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
 
-  return `hsla(${hash % 360}, ${s}%, ${l}%, ${val === 1 ? 0 : 0.6})`;
+  return `hsla(${hash % 360}, ${s}%, ${l}%, ${val ? 0.6 : 0})`;
 }
