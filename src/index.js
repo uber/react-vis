8c1a9fe36af239bc935162a783da874262ef4e8b
lhoff <jcust@leitkapture.com>
2019-02-11 20:15:13 -0600

    Alphabetize (#1105)

--- src/index.js
+++ src/index.js
@@ -18,62 +18,59 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-// TODO alphabetize
 export AbstractSeries from 'plot/series/abstract-series';
-export LineSeries from 'plot/series/line-series';
-export LineSeriesCanvas from 'plot/series/line-series-canvas';
+export ArcSeries from 'plot/series/arc-series';
+export AreaSeries from 'plot/series/area-series';
+export Borders from 'plot/borders';
+export ChartLabel from 'plot/chart-label';
+export CircularGridLines from 'plot/circular-grid-lines';
+export ContourSeries from 'plot/series/contour-series';
+export Crosshair from 'plot/crosshair';
+export CustomSVGSeries from 'plot/series/custom-svg-series';
+export DecorativeAxis from 'plot/axis/decorative-axis';
+export GradientDefs from 'plot/gradient-defs';
+export GridLines from 'plot/grid-lines';
+export HeatmapSeries from 'plot/series/heatmap-series';
+export HexbinSeries from 'plot/series/hexbin-series';
+export Highlight from 'plot/highlight';
+export Hint from 'plot/hint';
 export HorizontalBarSeries from 'plot/series/horizontal-bar-series';
 export HorizontalBarSeriesCanvas from 'plot/series/horizontal-bar-series-canvas';
-export VerticalBarSeries from 'plot/series/vertical-bar-series';
-export VerticalBarSeriesCanvas from 'plot/series/vertical-bar-series-canvas';
-export VerticalRectSeries from 'plot/series/vertical-rect-series';
-export VerticalRectSeriesCanvas from 'plot/series/vertical-rect-series-canvas';
+export HorizontalGridLines from 'plot/horizontal-grid-lines';
 export HorizontalRectSeries from 'plot/series/horizontal-rect-series';
 export HorizontalRectSeriesCanvas from 'plot/series/horizontal-rect-series-canvas';
 export LabelSeries from 'plot/series/label-series';
-export PolygonSeries from 'plot/series/polygon-series';
-export RectSeries from 'plot/series/rect-series';
-export RectSeriesCanvas from 'plot/series/rect-series-canvas';
-export MarkSeries from 'plot/series/mark-series';
-export MarkSeriesCanvas from 'plot/series/mark-series-canvas';
-export WhiskerSeries from 'plot/series/whisker-series';
-export HeatmapSeries from 'plot/series/heatmap-series';
-export HexbinSeries from 'plot/series/hexbin-series';
-export ContourSeries from 'plot/series/contour-series';
-export CustomSVGSeries from 'plot/series/custom-svg-series';
-export AreaSeries from 'plot/series/area-series';
-export ArcSeries from 'plot/series/arc-series';
 export LineMarkSeries from 'plot/series/line-mark-series';
 export LineMarkSeriesCanvas from 'plot/series/line-mark-series-canvas';
-export Hint from 'plot/hint';
-export Borders from 'plot/borders';
-export Crosshair from 'plot/crosshair';
+export LineSeries from 'plot/series/line-series';
+export LineSeriesCanvas from 'plot/series/line-series-canvas';
+export MarkSeries from 'plot/series/mark-series';
+export MarkSeriesCanvas from 'plot/series/mark-series-canvas';
+export PolygonSeries from 'plot/series/polygon-series';
+export VerticalBarSeries from 'plot/series/vertical-bar-series';
+export VerticalBarSeriesCanvas from 'plot/series/vertical-bar-series-canvas';
+export VerticalGridLines from 'plot/vertical-grid-lines';
+export VerticalRectSeries from 'plot/series/vertical-rect-series';
+export VerticalRectSeriesCanvas from 'plot/series/vertical-rect-series-canvas';
+export Voronoi from 'plot/voronoi';
+export RectSeries from 'plot/series/rect-series';
+export RectSeriesCanvas from 'plot/series/rect-series-canvas';
+export WhiskerSeries from 'plot/series/whisker-series';
 export XYPlot from 'plot/xy-plot';
-export DecorativeAxis from 'plot/axis/decorative-axis';
 export XAxis from 'plot/axis/x-axis';
 export YAxis from 'plot/axis/y-axis';
-export CircularGridLines from 'plot/circular-grid-lines';
-export ChartLabel from 'plot/chart-label';
-export GridLines from 'plot/grid-lines';
-export GradientDefs from 'plot/gradient-defs';
-export VerticalGridLines from 'plot/vertical-grid-lines';
-export HorizontalGridLines from 'plot/horizontal-grid-lines';
-export Voronoi from 'plot/voronoi';
-export Highlight from 'plot/highlight';
 
-// TODO alphabetize
-export DiscreteColorLegend from 'legends/discrete-color-legend';
-export SearchableDiscreteColorLegend from 'legends/searchable-discrete-color-legend';
 export ContinuousColorLegend from 'legends/continuous-color-legend';
 export ContinuousSizeLegend from 'legends/continuous-size-legend';
+export DiscreteColorLegend from 'legends/discrete-color-legend';
+export SearchableDiscreteColorLegend from 'legends/searchable-discrete-color-legend';
 
-// TODO alphabetize
-export Treemap from 'treemap';
-export RadialChart from 'radial-chart';
-export RadarChart from 'radar-chart';
 export ParallelCoordinates from 'parallel-coordinates';
+export RadarChart from 'radar-chart';
+export RadialChart from 'radial-chart';
 export Sankey from 'sankey';
 export Sunburst from 'sunburst';
+export Treemap from 'treemap';
 
 export {
   makeHeightFlexible,
