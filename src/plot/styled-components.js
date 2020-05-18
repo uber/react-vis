6879022be6ca48b74d911d2de877b0e563a8c3df
Chris Chua <chua@uber.com>
2020-05-17 18:08:22 -0700

    iteration 2

--- src/plot/styled-components.js
+++ src/plot/styled-components.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function XYPlotInnerSvgElement({className, ...props}) {
-  return <svg className={`rv-xy-plot__inner ${className}`} {...props} />;
+function styledWithClass(elementName, globalClassName) {
+  function StyledComponent({className, ...props}) {
+    return React.createElement(
+      elementName,
+      {className: `${globalClassName} ${className}`, ...props}
+    );
+  }
+  return styled(StyledComponent);
 }
-export const XYPlotInnerSvg = styled(XYPlotInnerSvgElement)`
+export const XYPlotInnerSvg = styledWithClass('svg', 'rv-xy-plot__inner')`
   display: block;
 `;
