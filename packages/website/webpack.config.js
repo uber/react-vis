a87aa94ea2ec61b35dde85550326b213bff49de8
Chris Thomas <tchrs@uber.com>
2020-05-23 11:45:21 -0400

    resolve to an older version of js-beautify that works with node@8

--- packages/website/webpack.config.js
+++ packages/website/webpack.config.js
@@ -2,10 +2,6 @@ const {resolve} = require('path');
 
 module.exports = {
   resolve: {
-    modules: [
-      resolve(__dirname, '../src'),
-      resolve(__dirname, '../node_modules')
-    ],
 
     alias: {
       react: resolve(__dirname, './node_modules/react')
