17b466d3c66ae9f74724190c0de5bc57f7edb46a
Chris Thomas <tchrs@uber.com>
2020-05-24 17:22:06 -0400

    revert jsdom upgrade

--- packages/react-vis/tests/setup.js
+++ packages/react-vis/tests/setup.js
@@ -1,14 +1,17 @@
 /* eslint-disable no-undef */
-import {JSDOM} from 'jsdom';
+import jsdom from 'jsdom';
 import Enzyme from 'enzyme';
 
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({adapter: new Adapter()})
 
-const dom = new JSDOM('<body></body>');
-const window = dom.window;
-global.document = window.document;
+global.document = jsdom.jsdom('<body></body>');
 global.window = document.defaultView;
+Object.keys(document.defaultView).forEach(function mapProperties(property) {
+  if (typeof global[property] === 'undefined') {
+    global[property] = document.defaultView[property];
+  }
+});
 
 global.navigator = {
   userAgent: 'node.js'
