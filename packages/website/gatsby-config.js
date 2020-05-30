0f5031ddea62b6d2b41790341c3743ee12d7d72b
Chris Chua <chua@uber.com>
2020-05-30 14:59:42 -0700

    Experiment: Test MDX format

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -1,6 +1,23 @@
 const {getGatsbyConfig} = require('ocular-gatsby/api');
-const util = require('util')
+const util = require('util');
 
 const config = require('./ocular-config');
-console.log('myconfig', util.inspect(getGatsbyConfig(config), {depth: null}))
-module.exports = getGatsbyConfig(config);
+console.log('myconfig', util.inspect(getGatsbyConfig(config), {depth: null}));
+const gatsbyConfig = getGatsbyConfig(config);
+gatsbyConfig.plugins.push('gatsby-plugin-mdx');
+
+/*
+TODO:
+Doesn't work for some reason
+gatsbyConfig.plugins.push({
+  resolve: 'gatsby-plugin-mdx',
+  gatsbyRemarkPlugins: [
+    // {resolve: 'gatsby-remark-images', options: {maxWidth: 690}},
+    // {resolve: 'gatsby-remark-responsive-iframe'},
+    // 'gatsby-remark-prismjs',
+    // 'gatsby-remark-copy-linked-files',
+    // 'gatsby-remark-autolink-headers'
+  ]
+});
+*/
+module.exports = gatsbyConfig;
