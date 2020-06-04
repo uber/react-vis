881dbe89e75a131539dca0f777c90f3b967c8435
Andrew McNutt <mcnutt.andrew@gmail.com>
2020-06-04 17:13:37 -0500

    Update problematic showcase examples to use hooks (#1345)
    * Update problematic showcase examples to use hooks
    
    * respond to requested changes

--- packages/showcase/examples/responsive-vis/responsive-bar-chart.js
+++ packages/showcase/examples/responsive-vis/responsive-bar-chart.js
@@ -48,59 +48,56 @@ function updateDataForArea(data, ppp) {
   return sample;
 }
 
-export default class ResponsiveBarChart extends React.Component {
-  // todo build a root responsive class that has this as a class method
-  getFeatures() {
-    const {data, height, margin, width} = this.props;
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-    const ppp = getPPP(innerWidth, innerHeight, data, 'HEIGHT');
-    return filterFeatures(BARCHART_FEATURES, ppp);
-  }
+export function getFeatures(props) {
+  const {data, height, margin, width} = props;
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+  const ppp = getPPP(innerWidth, innerHeight, data, 'HEIGHT');
+  return filterFeatures(BARCHART_FEATURES, ppp);
+}
 
-  render() {
-    const {data, height, margin, width} = this.props;
+export default function ResponsiveBarChart(props) {
+  const {data, height, margin, width} = props;
 
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-    const ppp = getPPP(innerWidth, innerHeight, data, 'HEIGHT');
-    const featuresToRender = filterFeatures(BARCHART_FEATURES, ppp);
-    const updatedData = featuresToRender.area
-      ? updateDataForArea(data, ppp)
-      : data;
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+  const ppp = getPPP(innerWidth, innerHeight, data, 'HEIGHT');
+  const featuresToRender = filterFeatures(BARCHART_FEATURES, ppp);
+  const updatedData = featuresToRender.area
+    ? updateDataForArea(data, ppp)
+    : data;
 
-    return (
-      <div className="responsive-bar-chart">
-        <XYPlot
-          yType="ordinal"
-          xType="linear"
-          margin={margin}
-          height={height}
-          width={width}
-        >
-          {featuresToRender.xaxis && <XAxis orientation="top" />}
-          {featuresToRender.yaxis && <YAxis />}
-          {featuresToRender.bars && (
-            <HorizontalBarSeries
-              colorType="literal"
-              yRange={[0, innerHeight]}
-              xRange={[0, innerWidth]}
-              data={updatedData}
-            />
-          )}
-          {featuresToRender.area && (
-            <AreaSeries
-              colorType="literal"
-              color="#12939A"
-              yType="linear"
-              yDomain={[0, updatedData.length]}
-              yRange={[0, innerHeight]}
-              xRange={[innerWidth, 0]}
-              data={updatedData}
-            />
-          )}
-        </XYPlot>
-      </div>
-    );
-  }
+  return (
+    <div className="responsive-bar-chart">
+      <XYPlot
+        yType="ordinal"
+        xType="linear"
+        margin={margin}
+        height={height}
+        width={width}
+      >
+        {featuresToRender.xaxis && <XAxis orientation="top" />}
+        {featuresToRender.yaxis && <YAxis />}
+        {featuresToRender.bars && (
+          <HorizontalBarSeries
+            colorType="literal"
+            yRange={[0, innerHeight]}
+            xRange={[0, innerWidth]}
+            data={updatedData}
+          />
+        )}
+        {featuresToRender.area && (
+          <AreaSeries
+            colorType="literal"
+            color="#12939A"
+            yType="linear"
+            yDomain={[0, updatedData.length]}
+            yRange={[0, innerHeight]}
+            xRange={[innerWidth, 0]}
+            data={updatedData}
+          />
+        )}
+      </XYPlot>
+    </div>
+  );
 }
