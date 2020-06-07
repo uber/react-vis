ee9858c4d961f5b2d879c27de3b038299a9fcfd9
apeltier <26309854+a-peltier@users.noreply.github.com>
2020-06-07 17:16:23 +0200

    Fix warningss in test for borders.test.js

--- packages/react-vis/tests/components/borders.test.js
+++ packages/react-vis/tests/components/borders.test.js
@@ -4,7 +4,19 @@ import Borders from 'plot/borders';
 import {testRenderWithProps, GENERIC_XYPLOT_SERIES_PROPS} from '../test-utils';
 import GradientExample from '../../../showcase/misc/gradient-example';
 
-testRenderWithProps(Borders, GENERIC_XYPLOT_SERIES_PROPS);
+testRenderWithProps(
+  Borders,
+  {
+    ...GENERIC_XYPLOT_SERIES_PROPS,
+    marginLeft: 0,
+    marginRight: 0,
+    innerWidth: 100,
+    marginTop: 0,
+    marginBottom: 0,
+    innerHeight: 100
+  },
+  true
+);
 
 describe('Borders', () => {
   test('GradientExample', () => {
